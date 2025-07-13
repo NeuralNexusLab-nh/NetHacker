85eac009507a9508e6fbc3472d508176979c99a4
NeuralNexusLab <neuralnexuslab@hotmail.com>
2025-07-13 16:15:41 +0800

    server.js

--- server.js
+++ server.js
@@ -5,12 +5,23 @@ const path = require("path");
 const app = express();
 
 app.set('trust proxy', true);
+
 app.use(express.json());
+app.use(cookie());
 
-app.get('/favicon.ico', (req, res) => {res.sendFile(path.join(__dirname, 'favicon.png'))});
-
-app.all('*', (req, res) => {
-  res.status(200).send(`server get a request from ${req.ip}, method is ${req.method}, headers is ${req.headers || none} and body is ${req.body || none}`);
+app.get('/favicon.ico', (req, res) => {
+  res.sendFile(path.join(__dirname, 'favicon.png'));
 });
 
-app.listen(process.env.PORT)
+app.all('*', (req, res) => {
+  res.status(200).send(
+    `Server received a request from ${req.ip}, method: ${req.method},\n` +
+    `headers: ${JSON.stringify(req.headers, null, 2)},\n` +
+    `body: ${JSON.stringify(req.body, null, 2)}`
+  );
+});
+
+const port = process.env.PORT || 443;
+app.listen(port, () => {
+  console.log(`Server is listening on port ${port}`);
+});
