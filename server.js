26792de0461ed8e671971240aeaf548bac55553d
NeuralNexusLab <neuralnexuslab@hotmail.com>
2025-07-13 16:22:28 +0800

    Update server.js

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ const express = require("express");
 const sql = require("mysql2");
 const cookie = require("cookie-parser");
 const path = require("path");
+const fs = require('fs');
 const app = express();
 
 app.set('trust proxy', true);
@@ -9,15 +10,20 @@ app.set('trust proxy', true);
 app.use(express.json());
 app.use(cookie());
 
+app.get('/', (req, res) => {
+  res.send('example text share website<br>try to GET "https://nethacker.onrender.com/submit/{ID_HERE}/{TEXT_HERE}" to submit<br>and GET https://nethacker.onrender.com/share/{ID_HERE} to view the text');
+};
+
+app.get('/submit/:id/:text', (req, res) => {
+});
+
 app.get('/favicon.ico', (req, res) => {
   res.sendFile(path.join(__dirname, 'favicon.png'));
 });
 
 app.all('*', (req, res) => {
-  res.status(200).send(
-    `Server received a request from ${req.ip}, method: ${req.method},\n` +
-    `headers: ${JSON.stringify(req.headers, null, 2)},\n` +
-    `body: ${JSON.stringify(req.body, null, 2)}`
+  res.status(404).send(
+    'ERROR 404: request path is not found!'
   );
 });
 
