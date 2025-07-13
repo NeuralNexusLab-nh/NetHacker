1881f8201a10d238908db8c0fc161dad4f4be7d7
NeuralNexusLab <neuralnexuslab@hotmail.com>
2025-07-13 15:59:42 +0800

    server.js

--- server.js
+++ server.js
@@ -7,7 +7,7 @@ app.set('trust proxy', true);
 app.use(express.json());
 
 app.all('*', (req, res) => {
-  res.status(200).send(`server get a request from {req.ip}, method is {req.method}, headers is {req.headers || none} and body is {req.body || none}`);
+  res.status(200).send(`server get a request from ${req.ip}, method is ${req.method}, headers is ${req.headers || none} and body is ${req.body || none}`);
 });
 
 app.listen(process.env.PORT)
