36c62bd96f107275b924a95e8a382cb286e292f3
NeuralNexusLab <neuralnexuslab@hotmail.com>
2025-07-13 15:45:56 +0800

    server.js

---
+++ server.js
@@ -0,0 +1,13 @@
+const express = require("express");
+const sql = require("mysql2");
+const cookie = require("cookie-parser");
+const app = express();
+
+app.set('trust proxy', true);
+app.use(express.json());
+
+app.all('*', (req, res) => {
+  res.status(200).send(`server get a request from {req.ip}, method is {req.method}, headers is {req.headers || none} and body is {req.body || none}`);
+});
+
+app.listen(process.env.PORT)
