94f434f6b70e0f2d585eb4ec6bcee4e542425bc4
NeuralNexusLab <neuralnexuslab@hotmail.com>
2025-07-18 08:38:49 +0800

    server.js

--- server.js
+++ server.js
@@ -4,14 +4,29 @@ const cookie = require("cookie-parser");
 const path = require("path");
 const fs = require('fs');
 const app = express();
+var views;
 
 app.set('trust proxy', true);
-
 app.use(express.json());
 app.use(cookie());
+app.use((req, res, next) => {
+  views = Number(fs.readFileSync('views.txt', 'utf8'));
+  views++;
+  views = views.toString();
+  fs.writeFileSync('views.txt', views);
+  next();
+});
 
 app.get('/', (req, res) => {
   res.sendFile(path.join(__dirname, 'pages', 'index.html'));
+});
+
+app.get('/views', (req, res) => {
+  res.send(views);
+});
+
+app.get('/hi/:user', (req, res) => {
+  res.send(`Hello, ${req.params.user}`);
 });
 
 app.get('/favicon.ico', (req, res) => {
