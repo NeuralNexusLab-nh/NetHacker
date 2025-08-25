42f408562054600eaf6f2e58bba8460840a83cb6
NeuralNexusLab <neuralnexuslab@hotmail.com>
2025-08-25 09:33:25 +0800

    Update server.js

--- server.js
+++ server.js
@@ -8,7 +8,7 @@ const app = express();
 //parameters
 const salt = process.env.SALT;
 const key = process.env.KEY;
-
+const user_agent = ["Firefox", "Chrome", "Edg", "Safari", "OPR", "CriOS", "FxiOS"]
 //functions
 function encrypt(text) {
   const cipher = crypto.createCipheriv("aes-256-ecb", key, null);
@@ -37,23 +37,41 @@ app.use(express.json());
 app.set("trust proxy", true);
 
 app.use((req, res, next) => {
-  if (!req.cookies.id) {
-    const id = crypto.randomBytes(16).toString("hex");
-    res.cookie("id", id, {
-      maxAge: 10000 * 60 * 60 * 24 * 365 * 10,
-      httpOnly: true,
-      secure: true,
-      sameSite: "lax"
-    })};
-  if (!req.cookies.pass) {
-      res.cookie("pass", pass(req.headers["user-agent"], req.ip, id), {
-      maxAge: 10000 * 60 * 60 * 24 * 365 * 10,
-      httpOnly: true,
-      secure: true,
-      sameSite: "lax"
-    });
+  var isAllowUa = false;
+  for (let i = 0; i < user_agent.length; i++) {
+    if (req.headers["user-agent"].include(user_agent[i])) {
+      isAllowUa = true;
+    }
+  }
+  if (isAllowUa) {
+      if (!req.cookies.id) {
+        const id = crypto.randomBytes(16).toString("hex");
+        res.cookie("id", id, {
+          maxAge: 10000 * 60 * 60 * 24 * 365 * 10,
+          httpOnly: true,
+          secure: true,
+          sameSite: "lax"
+        })};
+    if (!req.cookies.pass) {
+        res.cookie("pass", pass(req.headers["user-agent"], req.ip, id), {
+        maxAge: 10000 * 60 * 60 * 24 * 365 * 10,
+        httpOnly: true,
+        secure: true,
+        sameSite: "lax"
+      });
+      next();
+  } else {
+      res.sendFile("/error/403.html");
+    }
 });
 
+app.use((req, res, next) => {
+  if (req.cookies.pass !== pass(req.headers["user-agent"], req.ip, req.cookies.id) {
+    res.sendFile("/error/403.html")
+  }
+});
+
+      
 app.get("/", (req, res) => {
   res.sendFile("/pages/index.html");
 });
@@ -62,12 +80,7 @@ app.get("/style.css", (req, res) => {
   res.sendFile("/style.css");
 });
 
-app.get("/viral", (req, res) => {
-  res.sendFile("/pages/viral.html");
-});
 
-app.get("/reverseShell.py", (req, res) => {
-  res.send(fs.readFileSync("/reverseShell.py"));
-});
+
 
 app.listen(process.env.PORT, () => { console.log(`Server running at http://localhost:${process.env.PORT}.`) });
