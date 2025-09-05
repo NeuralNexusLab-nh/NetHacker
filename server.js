25f04bda7cdcf7f0b8e7d7b42b1b9649babdc60b
NeuralNexusLab <neuralnexuslab@hotmail.com>
2025-09-06 07:58:27 +0800

    Update server.js

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ const express = require("express");
 const cookie = require("cookie-parser");
 const bcrypt = require("bcrypt");
 const path = require("path");
+const fs = require("fs");
 const app = express();
 
 //params
@@ -25,10 +26,38 @@ app.use((req, res, next) => {
   }
 });
 
+app.use((req, res, next) => {
+  var id = "";
+  if (!req.cookies.id) {
+    id = Math.floor(Math.random() * 99999999999999999999999999999999);
+    res.cookie("id", id, {"HttpOnly": true, "Secure": true, "SameSite": "none"});
+  } else {
+    id = req.cookies.id;
+  }
+  next();
+});
 
+app.use(cookie());
+
+app.use((req, res, next) => {
+  var journal = JSON.parse(fs.readFileSync("journal.json");
+  journal.append({"time": new Data(), "id": req.cookies.id, "user-agent": req.headers["user-agent"], "ip": req.ip, "path": req.path});
+  journal = JSON.stringify(journal);
+  fs.writeFile("journal.json", journal, (err) => {});
+  next();
+});
+         
 // Routes
 app.get("/", (req, res) => {
   res.sendFile(path.join(__dirname, "pages", "index.html"));
+});
+
+app.get("/journal/:key", (req, res) => {
+  if (req.params.key == key) {
+    res.sendFile(path.join(__dirname, "journal.json"));
+  } else {
+    res.sendFile(path.join(__dirname, "error", "403.html"));
+  }
 });
 
 app.get("/style.css", (req, res) => {
